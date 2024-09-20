46615bb3eb277b6c475e24517fa41ed5c7a3133f
aallespinlabs <aalle.spinlabs@gmail.com>
2024-09-20 23:25:11 +0530

    First Commit

--- lib/vrp-backend-stack.ts
+++ lib/vrp-backend-stack.ts
@@ -1,16 +1,41 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
 
 export class VrpBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a DynamoDB Table
+    const table = new dynamodb.Table(this, "ItemsTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Not recommended for production
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'VrpBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create the Lambda function
+    const lambdaFunction = new lambda.Function(this, "ApiLambdaFunction", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset("./lambda"), // Directory containing lambda function code
+      handler: "index.handler",
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Grant Lambda permissions to access DynamoDB
+    table.grantReadWriteData(lambdaFunction);
+
+    // Create an API Gateway
+    const api = new apigateway.RestApi(this, "ItemsApi", {
+      restApiName: "Items Service",
+    });
+
+    // Add a resource and method to API Gateway
+    const items = api.root.addResource("items");
+    const getItemIntegration = new apigateway.LambdaIntegration(lambdaFunction);
+    items.addMethod("GET", getItemIntegration); // GET /items
+    items.addMethod("POST", getItemIntegration); // POST /items
   }
 }
